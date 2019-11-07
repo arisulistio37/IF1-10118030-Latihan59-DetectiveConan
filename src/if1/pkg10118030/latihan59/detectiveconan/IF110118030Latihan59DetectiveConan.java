6995e06b80c245d86be74a6388da072075906503
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:53:35 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan59/detectiveconan/IF110118030Latihan59DetectiveConan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan59.detectiveconan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi program : program ini berisi tentang membuat objek dari anime
+ *                     detective conan
+ */
+public class IF110118030Latihan59DetectiveConan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainCharacter conan = new MainCharacter("Investigator", "Menyelesaikan Kasus","Conan Edogawa","Tokyo",true);
+        MainCharacter ran = new MainCharacter("Karate", "Sama seperti Shinichi","Ran Mouri","Osaka",false);
+        SupportCharacter eisuke = new SupportCharacter("Teman Sekelas Ran Mouri","Eisuke Hondo","Osaka",false);
+        conan.displayCharacter();
+        ran.displayCharacter();
+        eisuke.displayCharacter();
+    }
+    
+}
